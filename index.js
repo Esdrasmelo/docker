3ae9ac504e54431ac8d6f3fae42de1945d2f51fc
Esdrasmelo <emelomotta@gmail.com>
2022-07-21 08:34:09 -0300

    adding more one route

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const appPort = 3000;
 app.get('/', (request, response) => {
     response.json({
         message: 'This express app is running on Docker!!',
-        tip: 'Go to the /docker route, please!'
+        availableRoutes: 'http://localhost:3000/author and http://localhost:3000/docker'
     });
 });
 
-app.get('/docker', (request, response) => {
-    response.redirect('https://github.com/Esdrasmelo');
-})
+app.get('/docker', (request, response) => response.redirect('https://www.docker.com/'));
+
+app.get('/author', (request, response) => response.redirect('https://github.com/Esdrasmelo'));
 
 app.listen(appPort, () => console.log(`Server is running at http://localhost:${appPort}`));
